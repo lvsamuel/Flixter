67b766059e952ea91a595640892334fb1bb0771e
lvsam <lucillevsam@gmail.com>
2017-10-26 01:57:15 +0000

    permissions added

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+  
     def show
     end
     
     private
+    def require_authorized_for_current_course
+      if current_course.user != current_user && ! current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You should be enrolled in this course!'
+      end
+    end
+    
     helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
     def enrolled_in?(course)
-    return enrolled_course.include?(course)
+      return enrolled_courses.include?(course)
     end
     
          
