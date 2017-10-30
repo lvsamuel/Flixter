c2bdd9830a4b6c775a08ae71ad51b604532d05b6
lvsam <lucillevsam@gmail.com>
2017-10-30 03:02:30 +0000

    Changes to permission

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,16 +1,16 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:show]
+  before_action :require_authorized_for_current_lesson, only: [:show]
   
     def show
     end
     
     private
-    def require_authorized_for_current_course
-      if current_course.user != current_user && ! current_user.enrolled_in?(current_course)
-      redirect_to course_path(current_course), alert: 'You should be enrolled in this course!'
-      end
+  def require_authorized_for_current_lesson
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You must be enrolled to view the lesson"
     end
+  end
     
     helper_method :current_lesson
   def current_lesson
